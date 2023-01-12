e7b56a4213363a7b4f98aad6cd20d22dcaef8ceb
kyoongdev <9898junjun2@gmail.com>
2023-01-12 15:13:57 +0900

    feature : posts

--- src/container/Home/Posts/index.tsx
+++ src/container/Home/Posts/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './posts.module.scss';
+import exampleImage from '../../../../public/assets/images/logo.png';
+import Image from 'next/image';
 
 const Post: React.FC = () => {
   return (
     <li className={styles.listItem}>
-      <div>ㅇㄹㅇㄹㅇㄹ</div>
+      <Image className={styles.thumbnail} src={exampleImage} alt='asdf' width={120} height={120} />
+      <header className={styles.listTitle}>오늘 공부하면서 느낀 점</header>
+      <ul className={styles.tags}>
+        <li>프런트엔드</li>
+        <li>인간관계</li>
+      </ul>
+      <p className={styles.description}>
+        아 개발 정말 재밌다...아 개발 정말 재밌다...아 개발 정말 재밌다...아 개발 정말 재밌다...아
+        개발 정말 재밌다...아 개발 정말 재밌다...
+      </p>
+      <footer>2023.01.12</footer>
     </li>
   );
 };
@@ -13,6 +25,13 @@ const Posts: React.FC = () => {
   return (
     <section className={styles.container}>
       <ul className={styles.listWrapper}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
         <Post />
       </ul>
     </section>
