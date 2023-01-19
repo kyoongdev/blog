6bd9382e417afe2fd6596980fdfb7adddb9d88b3
kyoongdev <9898junjun2@gmail.com>
2023-01-19 15:43:05 +0900

    feature : about page hr color:

--- src/container/About/MyInfo/index.tsx
+++ src/container/About/MyInfo/index.tsx
@@ -11,23 +11,25 @@ import School from 'assets/svg/school.svg';
 const MyInfo: React.FC = () => {
   return (
     <section className={styles.container}>
-      <article className={styles.myInfo}>
-        <h1>안녕하세요.</h1>
-        <div>
-          <h1>보이는 것, 그 이상을 찾는 개발자</h1>
-          <h1 className={styles.name}>박용준</h1>
-          <h1>입니다.</h1>
-        </div>
-      </article>
-      <article className={styles.description}>
-        <p>개발 능력을 갖추는 것을 넘어</p>
-        <p className={styles.values}>
-          <strong>소통</strong>,<strong>서비스의 목적 파악</strong>
-          <span /> 그리고
-          <strong>Self-Feedback</strong>
-        </p>
-        <p>을 할 수 있는 능력 갖추는 개발자가 되겠습니다.</p>
-      </article>
+      <section>
+        <article className={styles.myInfo}>
+          <h1>안녕하세요.</h1>
+          <div>
+            <h1>보이는 것, 그 이상을 찾는 개발자</h1>
+            <h1 className={styles.name}>박용준</h1>
+            <h1>입니다.</h1>
+          </div>
+        </article>
+        <article className={styles.description}>
+          <p>개발 능력을 갖추는 것을 넘어</p>
+          <p className={styles.values}>
+            <strong>소통</strong>,<strong>서비스의 목적 파악</strong>
+            <span /> 그리고
+            <strong>Self-Feedback</strong>
+          </p>
+          <p>을 할 수 있는 능력 갖추는 개발자가 되겠습니다.</p>
+        </article>
+      </section>
       <article className={styles.aboutMe}>
         <ul>
           <li>
@@ -50,6 +52,7 @@ const MyInfo: React.FC = () => {
             <Building />
             <p>CodeStates - FE Tutor (2023.01 ~ 2023.04)</p>
           </li>
+          <hr />
           <li>
             <Github />
             <p>Github</p>
