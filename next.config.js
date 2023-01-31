25a9a305ded002d68a8115f175cb158392d15459
kyoongdev <9898junjun2@gmail.com>
2023-01-31 23:42:54 +0900

    feature : image domains

--- next.config.js
+++ next.config.js
@@ -3,15 +3,14 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const removeImports = require('next-remove-imports')();
-const withImages = require('next-images');
 
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
   images: {
     domains: ['sgp1.vultrobjects.com'],
-    unoptimized: true,
   },
+  reactStrictMode: true,
+  trailingSlash: true,
+
   webpack(conf) {
     conf.optimization.splitChunks.cacheGroups = {};
     conf.optimization.minimize = true;
