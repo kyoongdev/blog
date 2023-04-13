18f38e4caff53b5f1340530ee1a8f4cddb6bb291
kyoongdev <9898junjun2@gmail.com>
2023-04-13 22:08:04 +0900

    chore : sitemap setting

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: 'https://kyoongdev.github.io',
+  siteUrl: 'https://blog.kyoongdev.com',
   generateRobotsTxt: true, // default: false, true 라고 설정해야 robots.txt 생성
   sitemapSize: 7000,
 };
