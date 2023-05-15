2f600590c8f6d6d1e98c39a62c640a7d77c48bf5
kyoongdev <9898junjun2@gmail.com>
2023-05-15 13:35:16 +0900

    chore(meta): canonical

--- src/components/HeadMeta/index.tsx
+++ src/components/HeadMeta/index.tsx
@@ -15,7 +15,10 @@ const HeadMeta: React.FC<Props> = ({ title, description, url, image, keywords })
       <title>{title || 'Kyoongdev Village'}</title>
       <meta
         name='description'
-        content={description || '보이지 않는 가치를 찾는 개발자, Kyoongdev의 기술 블로그입니다.'}
+        content={
+          description ||
+          '보이지 않는 가치를 찾는 개발자, Kyoongdev의 기술 블로그입니다. 개발 관련 글 및 좋은 개발자가 되기 위한 블로그 포스팅을 합니다.'
+        }
       />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta property='og:title' content={title || 'Kyoongdev Village'} />
@@ -26,9 +29,16 @@ const HeadMeta: React.FC<Props> = ({ title, description, url, image, keywords })
         content={image || 'https://sgp1.vultrobjects.com/kyoongdev-blog/images/me.jpg'}
       />
       <meta property='og:article:author' content='Kyoongdev' />
-      <meta name='description' content={description || 'Kyoongdev Village'} />
+      <link rel='canonical' href={url || 'https://blog.kyoongdev.com/'} />
       <meta name='author' content='Kyoongdev' />
-      <meta name='keyword' content={keywords ? keywords.join(', ') : '기술블로그, 포트폴리오'} />
+      <meta
+        name='keyword'
+        content={
+          keywords
+            ? keywords.join(', ')
+            : '기술블로그, 포트폴리오, 블로그, kyoongdev, kyoongdev blog, 백엔드 블로그, 프론트엔드 블로그'
+        }
+      />
     </Head>
   );
 };
