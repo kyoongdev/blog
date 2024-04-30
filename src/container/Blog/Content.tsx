88434550db3abfb9fc10b58b79bc8666b0ec99ba
kyoongdev <9898junjun2@gmail.com>
2024-04-30 21:42:40 +0900

    feat(blog): link blog

--- src/container/Blog/Content.tsx
+++ src/container/Blog/Content.tsx
@@ -1,6 +1,6 @@
 import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import React from 'react';
 
 import styles from './content.module.scss';
@@ -13,12 +13,6 @@ interface Props {
 }
 
 const Content: React.FC<Props> = ({ data }) => {
-  const router = useRouter();
-
-  const onClickResult: React.MouseEventHandler<HTMLLIElement> = (e) => {
-    const id = e.currentTarget.id;
-    router.push(`/blog/${id}`);
-  };
   return (
     <section className={styles.wrapper}>
       <ul className={styles.contents}>
@@ -27,16 +21,18 @@ const Content: React.FC<Props> = ({ data }) => {
           const cover = result.cover as DatabaseCover;
           return (
             <>
-              <li id={result.id} className={styles.content} onClick={onClickResult}>
-                <Image src={cover.external.url} alt='커버 이미지' width={160} height={200} />
-                <div>
-                  <div className={styles.header}>
-                    <h3>{property.Name.title[0].text.content}</h3>
-                    <Tags tags={property.Tags.multi_select.map((tag) => tag.name)} />
+              <Link href={`/blog/${result.id}`}>
+                <li id={result.id} className={styles.content}>
+                  <Image src={cover.external.url} alt='커버 이미지' width={160} height={200} />
+                  <div>
+                    <div className={styles.header}>
+                      <h3>{property.Name.title[0].text.content}</h3>
+                      <Tags tags={property.Tags.multi_select.map((tag) => tag.name)} />
+                    </div>
+                    <p className={styles.description}>{property.설명.rich_text[0].plain_text}</p>
                   </div>
-                  <p className={styles.description}>{property.설명.rich_text[0].plain_text}</p>
-                </div>
-              </li>
+                </li>
+              </Link>
               <hr className={styles.divider} />
             </>
           );
